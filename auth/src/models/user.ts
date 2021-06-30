e914e00923fb3b5733edb2bce8b10871dde2206c
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-07-01 01:25:12 +0300

    add mongoDB depl and work on mongoose User Model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that the User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
+
+User.build({
+  email: 43,
+});
+
+export { User };
